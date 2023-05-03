68567d6d72455fb3c02b1e626d794a9634ae6745
Dwiki <dwikis17@gmail.com>
2023-05-03 15:25:53 +0700

    Initial Commit

---
+++ Memorize/ContentView.swift
@@ -0,0 +1,27 @@
+//
+//  ContentView.swift
+//  Memorize
+//
+//  Created by Dwiki Dwiki on 03/05/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, CS193p")
+        }
+        .padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+        
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
